ab693acf6f21ac5ac5017b863cba373d764f8459
lmarch <lmarch@yandex.ru>
2024-11-22 18:00:11 +0300

    Creating area classes

---
+++ src/main/java/com/march/project/homework6/area/Forest.java
@@ -0,0 +1,7 @@
+package com.march.project.homework6.area;
+
+public class Forest extends Area {
+    public Forest(String areaName) {
+        super(areaName);
+    }
+}
---
+++ src/main/java/com/march/project/homework6/area/Plain.java
@@ -0,0 +1,7 @@
+package com.march.project.homework6.area;
+
+public class Plain extends Area{
+    public Plain(String areaName) {
+        super(areaName);
+    }
+}
