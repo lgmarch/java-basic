bcd6889b83ba3e8261ed97406972982fd4efb480
lgmarch <55672536+lgmarch@users.noreply.github.com>
2024-11-24 19:53:38 +0300

    Revert "Homework 6"

--- src/main/java/com/march/project/homework6/area/Forest.java
+++
@@ -1,7 +0,0 @@
-package com.march.project.homework6.area;
-
-public class Forest extends Area {
-    public Forest(String areaName) {
-        super(areaName);
-    }
-}
--- src/main/java/com/march/project/homework6/area/Plain.java
+++
@@ -1,7 +0,0 @@
-package com.march.project.homework6.area;
-
-public class Plain extends Area{
-    public Plain(String areaName) {
-        super(areaName);
-    }
-}
